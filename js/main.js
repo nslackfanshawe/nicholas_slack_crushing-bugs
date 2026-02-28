9438168c54af80b13f7ff0712fd74afd9a26b5f4
nslackfanshawe <n_slack@fanshaweonline.ca>
2026-02-28 02:36:39 -0500

    Created Base Files + Fixed Bug 1
    The base files have been created as well as bug 1 being fixed.

---
+++ js/main.js
@@ -0,0 +1,41 @@
+console.log("JavaScript File is linked");
+
+const labels = document.querySelectorAll(".label");
+const targetZones = document.querySelectorAll(".target-zone");
+let currentDraggedElement = null;
+
+//add variable for reset button;
+
+//event listeners
+labels.forEach(label => {
+    label.addEventListener("dragstart", dragStart);
+})
+
+function dragStart() {
+    console.log("Started Dragging");
+    currentDraggedElement = this;
+}
+
+targetZones.forEach(zone => {
+    zone.addEventListener("dragover", dragOver);
+    zone.addEventListener("drop", dropped);
+})
+
+function dragOver(e){
+    console.log("Drag over called")
+    e.preventDefault();
+}
+
+function dropped(e) {
+    e.preventDefault();
+    console.log("dropped");
+
+    
+    if(this.firstElementChild){
+        return;
+    }
+
+    this.appendChild(currentDraggedElement);
+    currentDraggedElement = null;
+}
+
